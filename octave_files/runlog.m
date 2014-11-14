948ebe2b4cb3826236d5b51f3dd8a861bb6cba28
domspad <domspad@umich.edu>
2014-11-14 13:24:05 -0500

    after first simple algorithm ran: logreg

---
+++ octave_files/runlog.m
@@ -0,0 +1,54 @@
+%% Initialization
+clear ; close all; clc
+
+
+%% Load Data
+%  The first two columns contains the exam scores and the third column
+%  contains the label
+data = load('train1.txt');
+Xtrain = data(1:691, 1:9); ytrain = data(1:691, 10);
+Xcv = data(692:end,1:9); ycv = data(692:end,10);
+
+%% ============ Part 2: Compute Cost and Gradient ============
+
+%  Setup the data matrix appropriately, and add ones for the intercept term
+[m, n] = size(Xtrain);
+
+% Add intercept term to x and X_test
+Xtrain = [ones(m, 1) Xtrain];
+Xcv = [ones(size(Xcv,1),1) Xcv];
+
+% Initialize fitting parameters
+initial_theta = zeros(n + 1, 1);
+
+% Compute and display initial cost and gradient
+[cost, grad] = costFunction(initial_theta, Xtrain, ytrain);
+
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+fprintf('Gradient at initial theta (zeros): \n');
+fprintf(' %f \n', grad);
+
+
+%% ============= Part 3: Optimizing using fminunc  =============
+
+
+options = optimset('GradObj', 'on', 'MaxIter', 400);
+
+[theta, cost] = ...
+	fminunc(@(t)(costFunction(t, Xtrain, ytrain)), initial_theta, options);
+
+fprintf('Cost at theta found by fminunc: %f\n', cost);
+fprintf('theta: \n');
+fprintf(' %f \n', theta);
+
+%% ============== Part 4: Predict and Accuracies ==============
+
+% Compute accuracy on our training set
+ptrain = predict(theta, Xtrain);
+fprintf('Train Accuracy: %f\n', mean(double(ptrain == ytrain)) * 100);
+fprintf('Train cost %f\n', costFunction(theta, Xtrain, ytrain));;
+
+
+pcv = predict(theta, Xcv);
+fprintf('CV Accuracy: %f\n', mean(double(pcv == ycv)) * 100);
+fprintf('CV cost %f\n', costFunction(theta, Xcv, ycv));
